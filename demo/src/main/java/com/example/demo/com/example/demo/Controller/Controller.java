e4bcbb34013a217569ea4f2869290b3822023d08
Mohammad Ameen <contactameen25@gmail.com>
2024-02-23 13:30:26 +0530

    changes in put to patch

--- demo/src/main/java/com/example/demo/com/example/demo/Controller/Controller.java
+++ demo/src/main/java/com/example/demo/com/example/demo/Controller/Controller.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -48,7 +49,7 @@ public class Controller {
 		return service.getEmployeeByDeptarment();
 	}
 
-	@PutMapping("/inactivate-activate-employee")
+	@PatchMapping("/inactivate-activate-employee")
 	public Result inactivateActivateEmployee(@RequestBody ActiveInactiveRequestBody activeInactiveRequestBody)   {
 		return service.inactivateActivateEmployee(activeInactiveRequestBody);
 	}
